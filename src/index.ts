5d86455695e633515dd35542068f7c89582e9617
Igor Tsuker <igorek888rane@gmail.com>
2023-01-28 21:09:30 +0300

    WebSocket connect

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
 import * as dotenv from 'dotenv'
 import express from 'express'
+import cors from 'cors'
+import expressWs from 'express-ws'
 
 dotenv.config()
 
-const app = express()
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,()=>console.log(`Server started on port ${PORT}`))
+const app = express()
+const WSServer = expressWs(app)
+const aWss = WSServer.getWss()
+
+
+
+app.use(cors())
+app.use(express.json())
+
+WSServer.app.ws('/',(ws,req)=>{
+console.log('Connect')
+    ws.send('Successful connect')
+    ws.on('message',(msg)=>{
+        console.log(msg)
+    })
+})
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
